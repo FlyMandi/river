dfc6c07546bcb3fd7b92e953fc53bdb643e66bf8
Federico <steidlmartinez@gmail.com>
2024-11-27 02:03:33 +0100

    fix misc

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <mat4x4.hpp>
-#include <vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
 
 #include <iostream>
 
