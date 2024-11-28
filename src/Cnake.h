e43a3c61f18b0764fe49f342c24e4f8cd1213027
Federico <steidlmartinez@gmail.com>
2024-11-28 12:55:27 +0100

    add version to window name

--- src/Cnake.h
+++ src/Cnake.h
@@ -1,18 +1,22 @@
 #pragma once
 
 #include "Engine_Window.h"
+#include <string>
 
 namespace engine{
 
 class Cnake {
  public:
-    static constexpr int w_WIDTH = 1280;
-    static constexpr int w_HEIGHT = 720;
+    static constexpr int w_WIDTH = 1920;
+    static constexpr int w_HEIGHT = 1080;
 
     void run();
 
  private:
-    EngineWindow engineWindow{w_WIDTH, w_HEIGHT, "I did it!"};
+    const std::string m_Version = "0.0.1";
+
+ private:
+    EngineWindow engineWindow{w_WIDTH, w_HEIGHT, "Cnake " + m_Version}; 
 };
 
 }
