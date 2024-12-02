82c8b7e0a15a1c69daf61268bca2b1b53937ed54
Federico <steidlmartinez@gmail.com>
2024-12-02 02:38:44 +0100

    tutorial progress

--- src/Engine_Pipeline.cpp
+++ src/Engine_Pipeline.cpp
@@ -1,13 +1,20 @@
 #include "Engine_Pipeline.h"
+#include "Engine_Device.h"
+#include "vulkan/vulkan_core.h"
 
+#include <cstdint>
 #include <fstream>
 #include <stdexcept>
 #include <iostream>
 
 namespace engine{
 
-EnginePipeline::EnginePipeline(const std::string &vertFilepath, const std::string &fragFilepath){
-    createGraphicsPipeline(vertFilepath, fragFilepath);
+EnginePipeline::EnginePipeline(
+        class EngineDevice &device, 
+        const PipelineConfigInfo &configInfo, 
+        const std::string &vertFilepath, 
+        const std::string &fragFilepath) : EngineDevice{device} {
+    createGraphicsPipeline(configInfo, vertFilepath, fragFilepath);
 }
 
 std::vector<char> EnginePipeline::readFile(const std::string &filepath){
@@ -27,12 +34,30 @@ std::vector<char> EnginePipeline::readFile(const std::string &filepath){
     return buffer;
 }
 
-void EnginePipeline::createGraphicsPipeline(const std::string &vertFilepath, const std::string &fragFilepath){
+void EnginePipeline::createGraphicsPipeline(const PipelineConfigInfo &configInfo,const std::string &vertFilepath, const std::string &fragFilepath){
     auto vertCode = readFile(vertFilepath);
     auto fragCode = readFile(fragFilepath);
 
     std::cout << "Vertex Shader Code Size: " << vertCode.size() << '\n';
     std::cout << "Frag Shader Code Size: " << fragCode.size() << '\n';
+}
+
+void EnginePipeline::createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule){
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = code.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+    if (vkCreateShaderModule(EngineDevice.device(), &createInfo, nullptr, shaderModule) != VK_SUCCESS){
+        throw std::runtime_error("failed to create shader module.");
+    }
+
+}
+
+PipelineConfigInfo EnginePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
+    PipelineConfigInfo configInfo{};
+
+    return configInfo;
 }
 
 } //namespace engine
--- src/Engine_Pipeline.h
+++ src/Engine_Pipeline.h
@@ -1,18 +1,38 @@
 #pragma once
 
+#include "Engine_Device.h"
+#include "vulkan/vulkan_core.h"
+
+#include <cstdint>
 #include <vector>
 #include <string>
 
 namespace engine{
 
+struct PipelineConfigInfo{};
+
 class EnginePipeline{
  public:
-    EnginePipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+    EnginePipeline(EngineDevice &device, const PipelineConfigInfo &configInfo, const std::string &vertFilepath, const std::string &fragFilepath);
+
+    ~EnginePipeline(){}
+
+    EnginePipeline(const EnginePipeline&) = delete;
+    EnginePipeline operator=(const EnginePipeline&) = delete;
+
+    static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
  private:
     static std::vector<char> readFile(const std::string &filepath);
 
-    void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+    void createGraphicsPipeline(const PipelineConfigInfo &configInfo,const std::string &vertFilepath, const std::string &fragFilepath);
+
+    void createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule);
+
+    EngineDevice &EngineDevice;
+    VkPipeline graphicsPipeline;
+    VkShaderModule vertShaderModule;
+    VkShaderModule fragShaderModule;
 };
 
 }  //namespace engine 
