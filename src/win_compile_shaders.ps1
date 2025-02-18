1a7e57cdfcc228429b3b19987d1f9155f6ab17b0
Federico <steidlmartinez@gmail.com>
2025-02-18 20:30:09 +0100

    make sure shaders folder exists

--- src/win_compile_shaders.ps1
+++ src/win_compile_shaders.ps1
@@ -1,15 +1,18 @@
 $glslc = (Join-Path -PATH $env:VULKAN_SDK -ChildPath "\Bin\glslc.exe")
+$shaders =  (Join-Path -PATH $PSScriptRoot -ChildPath "\shaders\")
+
+if(-Not(Test-Path $shaders)){ &mkdir $shaders }
 
 function Out-Shader{
     param(
         $fileName
     )
-    $source = (Join-Path -PATH $PSScriptRoot -ChildPath "\shaders\$fileName")
+    $source = Join-Path -Path $shaders -ChildPath $fileName
     $target = "$source.spv"
     $param = $source, '-o', $target 
     
     &$glslc $param
-    Write-Host "        Compiled " -NoNewline
+    Write-Host "Compiled " -NoNewline
     Write-Host $fileName -BackgroundColor DarkCyan -NoNewline
     Write-Host " successfully to: $target"
 }
