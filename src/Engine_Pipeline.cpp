4b8620ea5375d3379847f725233af8936b98eecf
Federico <steidlmartinez@gmail.com>
2025-03-06 15:38:44 +0100

    discover J in neovim

--- src/Engine_Pipeline.cpp
+++ src/Engine_Pipeline.cpp
@@ -20,9 +20,7 @@ EnginePipeline::EnginePipeline(
 std::vector<char> EnginePipeline::readFile(const std::string &filepath){
     std::ifstream file{filepath, std::ios::ate | std::ios::binary};
 
-    if (!file.is_open()){
-        throw std::runtime_error("failed to open file: " + filepath);
-    }
+    if(!file.is_open()){ throw std::runtime_error("failed to open file: " + filepath); }
 
     size_t fileSize = static_cast<size_t>(file.tellg());
     std::vector<char> buffer(fileSize);
