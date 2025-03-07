043f84dfd19605f24037e3ae6876964cb33ef04b
Federico <steidlmartinez@gmail.com>
2025-03-07 20:19:52 +0100

    add default pipelineConfigInfo values

--- src/Engine_Pipeline.cpp
+++ src/Engine_Pipeline.cpp
@@ -50,6 +50,38 @@ void EnginePipeline::createShaderModule(const std::vector<char> &code, VkShaderM
 PipelineConfigInfo EnginePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
     PipelineConfigInfo configInfo{};
 
+    configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE; 
+
+    configInfo.viewport.x = 0.0f;
+    configInfo.viewport.y = 0.0f;
+    configInfo.viewport.width = static_cast<float>(width);
+    configInfo.viewport.height = static_cast<float>(height);
+    configInfo.viewport.minDepth = 0.0f;
+    configInfo.viewport.maxDepth = 1.0f;
+    
+    configInfo.scissor.offset = {0, 0};
+    configInfo.scissor.extent = {width, height};
+
+    configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    configInfo.viewportInfo.viewportCount = 1;
+    configInfo.viewportInfo.pViewports = &configInfo.viewport;
+    configInfo.viewportInfo.scissorCount = 1;
+    configInfo.viewportInfo.pScissors = &configInfo.scissor;
+
+    configInfo.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
+    configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+    configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
+    configInfo.rasterizationInfo.lineWidth = 1.0f;
+    configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_NONE;
+    configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
+    configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;
+    configInfo.rasterizationInfo.depthBiasClamp = 0.0f;
+    configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f;
+
     return configInfo;
 }
 
