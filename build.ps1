4e037951444cc1279d074da41595c62b54377c6a
Federico <steidlmartinez@gmail.com>
2025-03-10 14:30:31 +0100

    back to build selection

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
+param( $build = "debug" )
 &premake5 vs2022
-.\src\win_compile_shaders.ps1
-&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=debug
-&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=release
+&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=$build
+.\win_compile_shaders.ps1
