7330e1867216024ae86f4afbd0b3dfc389de325f
Federico <steidlmartinez@gmail.com>
2025-03-11 15:10:19 +0100

    add debug callback & debug messenger

--- src/h/cnake.h
+++ src/h/cnake.h
@@ -5,13 +5,14 @@
 #include "vulkan/vulkan_core.h"
 
 #include <vector>
+#include <iostream>
 
 class CnakeApp{
     const char *version = "Cnake 0.0.0";
     const uint32_t WIDTH = 1920;
     const uint32_t HEIGHT = 1080;
 
-    uint32_t extensionCount = 0;
+    uint32_t instanceExtensionCount = 0;
     uint32_t glfwExtensionCount = 0;
 
     const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
@@ -24,9 +25,26 @@ class CnakeApp{
 
     GLFWwindow *window;
     VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
 
-    bool checkExtensionSupport(const char **glfwExt, const std::vector<VkExtensionProperties> *vulkanExt);
+    std::vector<const char*> getRequiredExtensions();
+    
+    bool checkExtensionSupport(std::vector<const char*> *requiredExt, std::vector<VkExtensionProperties> *instanceExt);
     bool checkValidationLayerSupport();
+
+    void setupDebugMessenger();
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT             messageType, 
+        const VkDebugUtilsMessengerCallbackDataEXT  *pCallbackData,
+        void                                        *pUserData
+        ){
+
+        std::cerr << "validation layer: " << pCallbackData->pMessage << '\n';
+
+        return VK_FALSE;
+    }
 
     void initWindow();
     void createInstance();
