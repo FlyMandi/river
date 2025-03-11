ea3f4099d7f70da2d0a3e985d2ddba53556ba091
Federico <steidlmartinez@gmail.com>
2025-03-11 18:48:52 +0100

    move stuff around

--- src/h/cnake.h
+++ src/h/cnake.h
@@ -3,16 +3,16 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <cstdint>
-
 class CnakeApp{
     const char *version = "Cnake 0.0.0";
     const uint32_t WIDTH = 1920;
     const uint32_t HEIGHT = 1080;
 
+    //TODO: make sure this points to the same exact window as Engine::window
     GLFWwindow *window;
 
     //TODO: remove copy constructor & operator
+    //deal with constructors & destructors, default constructors
 
     void initWindow();
     void gameLoop();
