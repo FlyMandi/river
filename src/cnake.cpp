62df9a0f8d356ace5e51eed91e6ddcdc59049f64
Federico <steidlmartinez@gmail.com>
2025-03-11 15:27:03 +0100

    add proxy functions for creating & destroying the debug messenger

--- src/cnake.cpp
+++ src/cnake.cpp
@@ -7,6 +7,33 @@
 #include <vector>
 #include <cstring>
 
+VkResult CreateDebugUtilsMessengerEXT(
+        VkInstance                                  instance,
+        const VkDebugUtilsMessengerCreateInfoEXT    *pCreateInfo,
+        const VkAllocationCallbacks                 *pAllocator,
+        VkDebugUtilsMessengerEXT                    *pDebugMessenger
+    ){
+
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if(nullptr != func){
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    }else{
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void DestroyDebugUtilsMessengerEXT(
+        VkInstance                  instance,
+        VkDebugUtilsMessengerEXT    messenger, 
+        const VkAllocationCallbacks *pAllocator
+    ){
+
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if(nullptr != func){
+        func(instance, messenger, pAllocator);
+    }
+}
+
 std::vector<const char*> CnakeApp::getRequiredExtensions(){
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions;
@@ -50,7 +77,6 @@ bool CnakeApp::checkExtensionSupport(std::vector<const char*> *requiredExt, std:
     }
 
     std::cout << "\n\tRequired:\n"; 
-    
     for(const auto &required : *requiredExt){
         bool extFound = false;
         
@@ -135,6 +161,10 @@ void CnakeApp::setupDebugMessenger(){
     createInfo.messageSeverity =    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT; 
     createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
     createInfo.pfnUserCallback = debugCallback;
+
+    if(CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger)){
+        throw std::runtime_error("failed to set up debug messenger!");
+    }
 }
 
 void CnakeApp::gameLoop(){
@@ -144,6 +174,10 @@ void CnakeApp::gameLoop(){
 }
 
 void CnakeApp::cleanup(){
+    if(enableValidationLayers){
+        DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    }
+
     vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
