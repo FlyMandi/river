d970224d40066bb5e770f2a5dc34ae27af0be6fa
Federico <steidlmartinez@gmail.com>
2025-03-12 12:12:32 +0100

    remove forced rebuild

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 param( $build = "debug" )
 &premake5 vs2022
-&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=$build
+&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -p:Configuration=$build
 .\win_compile_shaders.ps1
