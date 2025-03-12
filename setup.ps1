86a5090753e962e0bfedf975f08c1c4ee4b14e4f
Federico <steidlmartinez@gmail.com>
2025-03-12 20:47:34 +0100

    add more todo

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,7 @@
 #TODO: install VulkanSDK if not present
 
-#TODO: install premake5 if not present
+#TODO: check for scoop and install premake5 that way
+
+#TODO: install premake5 if not present 
 
 #TODO: install and update git submodules?
