b72f0bebdc95af95bd7aa7e09897c13cb7bdae9a
Federico <steidlmartinez@gmail.com>
2025-03-13 02:31:38 +0100

    add premake5 automated installation

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,18 @@
 #TODO: install VulkanSDK if not present
+#curl -O https://sdk.lunarg.com/sdk/download/latest/windows/vulkan_sdk.exe
 
-#TODO: check for scoop and install premake5 that way
+if(-Not(Get-Command premake5.exe -ErrorAction SilentlyContinue)){
+   if(Get-Command scoop -ErrorAction SilentlyContinue){ 
+        &scoop install premake5
+   }else{
+        $sourceRepo = "premake/premake-core"
+        $namePattern = "*windows.zip"
+        $sourceURI = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/$sourceRepo/releases/latest").assets | Where-Object name -like $namePattern).browser_download_url
+        $zipFolderName = $(Split-Path -Path $sourceURI -Leaf)
+        $tempZIP = Join-Path -Path $([System.IO.Path]::GetTempPath()) -ChildPath $zipFolderName 
+        Invoke-WebRequest -Uri $sourceURI -Out $tempZIP
 
-#TODO: install premake5 if not present 
-
-#TODO: install and update git submodules?
+        Expand-Archive -Path $tempZIP -DestinationPath $PSScriptRoot -Force
+        Remove-Item $tempZIP -Force
+   }
+}
