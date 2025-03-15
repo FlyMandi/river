9907aaecf082ed7aea613955f6d984f2f1fc0546
Federico <steidlmartinez@gmail.com>
2025-03-15 12:17:55 +0100

    lint

--- src/h/window.h
+++ src/h/window.h
@@ -10,14 +10,14 @@ inline const char *appName = "Cnake";
 inline const char *engineName = "CNNgine";
 inline const char *version = "0.0.1";
 
-inline std::filesystem::path appRoot = getProjectRoot(appName);
-
 inline const uint32_t WIDTH = 1920;
 inline const uint32_t HEIGHT = 1080;
 
 inline bool window_SHOULDCLOSE = false;
 
 inline GLFWwindow *window;
+
+inline std::filesystem::path appRoot = getProjectRoot(appName);
 
 void initGLFW();
 void cleanupGLFW();
