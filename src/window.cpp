dd94a1fce7be3f281558e0f213f8e836a370c269
Federico <steidlmartinez@gmail.com>
2025-03-18 22:32:02 +0100

    TODO updates

--- src/window.cpp
+++ src/window.cpp
@@ -1,15 +1,22 @@
+#include "engine.h"
 #include "river.h"
 #include "window.h"
 
+#include <stdexcept>
 #include <string>
 
 void window::initGLFW(){
     glfwInit();
 
+    //TODO: add window resizeability
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     pWindow = glfwCreateWindow(WIDTH, HEIGHT, ((std::string)river::appName + " " + (std::string)river::appVersion).c_str(), nullptr, nullptr);
+    if(nullptr == pWindow){
+        engine::printDebugLog("\nERROR: failed to create GLFW window!", 0, 1);
+        throw std::runtime_error("failed to create GLFW window!");
+    }
 }
 
 void window::cleanupGLFW(){
