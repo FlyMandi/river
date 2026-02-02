d1866daa6140abdbff4b398f454e28014aa04ee1
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:11 +0100

    lint

--- src/image.cpp
+++ src/image.cpp
@@ -42,8 +42,7 @@ internal void copyBufferToImage
     vkCmdCopyBufferToImage
     (
         commandBuffer,
-        buffer,
-        image,
+        buffer, image,
         VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
         1,
         &region
