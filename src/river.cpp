5352b65a63a6b7213ff4cca70b9ef9220ba5eaa9
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:07 +0100

    add framebuffer resize check

--- src/river.cpp
+++ src/river.cpp
@@ -278,7 +278,8 @@ void drawFrame(){
 
     result = vkQueuePresentKHR(presentQueue, &presentInfo);
 
-    if(result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR){
+    if(result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || framebufferResized){
+        framebufferResized = false;
         recreateSwapChain();
 
     }else if(result != VK_SUCCESS){
