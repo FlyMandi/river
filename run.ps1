75881085c7c667dc4937902219eae2df424ecea5
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    modular path

--- run.ps1
+++ run.ps1
@@ -2,5 +2,5 @@ param( $config = "release" )
 &premake5 clean
 &premake5 vs2022
 .\src\win_compile_shaders.ps1
-&"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=$config
+&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=$config
 &".\bin\$config\Cnake.exe"
