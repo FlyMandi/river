aa00613d51a00a7478c716a926840f944da6384e
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:09 +0100

    create texture image

--- src/image.cpp
+++ src/image.cpp
@@ -1,6 +1,54 @@
+#include "vulkan/vulkan_core.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "river.h"
 #include "image.h"
+#include "buffer.h"
+#include "device.h"
 
 void createTextureImage()
 {
+    int texWidth;
+    int texHeight;
+    int texChannels;
 
+    stbi_uc *pixels =   stbi_load
+                        (
+                            "assets/textures/texture.jpg",
+                            &texWidth,
+                            &texHeight,
+                            &texChannels,
+                            STBI_rgb_alpha
+                        );
+
+    riverAssert(pixels, "failed to load texture image!");
+
+    VkDeviceSize imageSize = texWidth * texHeight * 4;
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+
+    std::set<uint32_t> uniqueFamilyIndices =
+    {
+        logicalQueueFamilies.graphicsIndex,
+        logicalQueueFamilies.transferIndex
+    };
+
+    createBuffer
+    (
+        imageSize,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory,
+        uniqueFamilyIndices
+    );
+
+    void* data;
+    vkMapMemory(logicalDevice, stagingBufferMemory, 0, imageSize, 0, &data);
+    std::memcpy(data, pixels, static_cast<size_t>(imageSize));
+    vkUnmapMemory(logicalDevice, stagingBufferMemory);
+
+    stbi_image_free(pixels);
 }
