a3236d29c9265c3276e0c0a0af56136a305748b4
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    don't use push_back

--- src/engine.cpp
+++ src/engine.cpp
@@ -119,7 +119,7 @@ std::vector<const char*> getRequiredExtensions(){
     std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
     if(enableValidationLayers){
-        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     }
 
     return extensions;
