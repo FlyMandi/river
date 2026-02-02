44d0f4a7c805c915e6f5df1acdbe8d0f72bac43e
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:08 +0100

    createDescriptorSetLayout()

--- include/river.h
+++ include/river.h
@@ -29,6 +29,7 @@ extern void initVulkan();
 extern void cleanupVulkan();
 
 extern void drawFrame();
+extern void createDescriptorSetLayout();
 
 extern std::filesystem::path getProjectRoot(const char *rootName);
 extern void clearLogs(const std::filesystem::path &baseDir);
