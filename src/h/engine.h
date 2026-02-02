18bdc50120792c0a55fb22acbe69aabf5788c8a1
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:42:15 +0100

    shenanigans

--- src/h/engine.h
+++ src/h/engine.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#define GLFW_INCLUDE_VULKAN
 #include "vulkan/vulkan_core.h"
 
 #include <cstdlib>
@@ -8,47 +7,6 @@
 #include <iostream>
 #include <vector>
 #include <optional>
-
-#ifdef NDEBUG
-    const bool build_DEBUG = false;
-#else 
-    const bool build_DEBUG = true;
-#endif
-
-inline const char *engineName = "River";
-
-inline const int MAX_FRAMES_IN_FLIGHT = 2;
-
-inline VkInstance instance;
-inline VkDebugUtilsMessengerEXT debugMessenger;
-
-inline VkPhysicalDevice physicalDevice;
-inline VkPhysicalDeviceProperties deviceProperties;
-inline VkPhysicalDeviceFeatures deviceFeatures;
-
-inline VkDevice device;
-
-inline VkQueue graphicsQueue;
-inline VkQueue presentQueue;
-
-inline VkSurfaceKHR surface;
-inline VkSwapchainKHR swapChain;
-inline VkFormat swapChainImageFormat;
-inline VkExtent2D swapChainExtent;
-
-inline std::vector<VkImage> swapChainImages;
-inline std::vector<VkImageView> swapChainImageViews;
-inline std::vector<VkFramebuffer> swapChainFramebuffers;
-
-inline VkRenderPass renderPass;
-inline VkPipelineLayout pipelineLayout;
-inline VkPipeline graphicsPipeline;
-inline VkCommandPool commandPool;
-inline VkCommandBuffer commandBuffer;
-
-inline VkSemaphore imageAvailableSemaphore;
-inline VkSemaphore renderFinishedSemaphore;
-inline VkFence inFlightFence;
 
 void createInstance();
 void setupDebugMessenger();
--- src/h/window.h
+++ src/h/window.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <GLFW/glfw3.h>
+#include "GLFW/glfw3.h"
 
 #include "utils.h"
 
@@ -12,9 +12,9 @@ inline const char *version = "0.0.1";
 inline const uint32_t WIDTH = 1920;
 inline const uint32_t HEIGHT = 1080;
 
-inline bool window_SHOULDCLOSE = false;
-
 inline GLFWwindow *window;
+
+inline bool window_SHOULDCLOSE = false;
 
 inline std::filesystem::path appRoot = getProjectRoot(appName);
 
