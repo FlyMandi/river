a0c51b0e5e72bc56bd39818799ee6f86d0f8a2b6
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:11 +0100

    add IWYU pragma: keep explanation

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -11,7 +11,7 @@
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/trigonometric.hpp"
-#include "glm/gtx/hash.hpp" // IWYU pragma: keep
+#include "glm/gtx/hash.hpp" // IWYU pragma: keep (for some reason clang doesn't recognize I need it below)
 #include "vulkan/vulkan_core.h"
 
 #include "pipeline.h"
