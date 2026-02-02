2e19b9dc563354f785d2b5f8871a99f6af30e4d1
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:08 +0100

    vulkan-sdk package has been deprecated

--- setup.ps1
+++ setup.ps1
@@ -26,18 +26,11 @@ function Install-VulkanSDK{
         Write-Host "`nExpect a UAC prompt." 
         &.\VulkanSDK-$installVersion-Installer.exe
     }elseif($isLinux){
-        if(Get-Command apt-get -ErrorAction SilentlyContinue){
-            Write-Host "Expect a sudo prompt. If you don't like that or it fails, manually install via:"
-            Write-Host "`nsudo apt-get install vulkan-sdk=$installVersion"
-            &sudo apt-get install vulkan-sdk=$installVersion
-        }else{
-            Write-Host "You don't have apt-get, so install via the package manager of your choice or proceed below."
-            &curl -O https://sdk.lunarg.com/sdk/download/$installVersion/linux/vulkansdk-linux-x86_64-$installVersion.tar.xz
-            Write-Host "`nVulkanSDK version $installVersion has been downloaded, but not installed."
-            Write-Host "Now install the SDK the rest of the way." 
-            Write-Host "`nIf you need help, read the article below:"
-            Write-Host "https://www.amd.com/en/resources/support-articles/faqs/GPU-636.html"
-        }
+        &curl -O https://sdk.lunarg.com/sdk/download/$installVersion/linux/vulkansdk-linux-x86_64-$installVersion.tar.xz
+        Write-Host "`nVulkanSDK version $installVersion has been downloaded, but not installed."
+        Write-Host "Now install the SDK the rest of the way." 
+        Write-Host "`nIf you need help, read the article below:"
+        Write-Host "https://www.amd.com/en/resources/support-articles/faqs/GPU-636.html"
 
     }elseif($IsMacOS){
         #TODO: add mac install steps... eh.
