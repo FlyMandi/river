0216d7482e684d7047173d90ba3f58c8ec9a6d28
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:42:44 +0100

    refactor functions that can be static

--- src/utils.cpp
+++ src/utils.cpp
@@ -26,33 +26,6 @@ void river::getProjectRoot(const char *rootName){
     }
 }
 
-std::vector<char> engine::readFile(const std::string &filename){
-    std::ifstream file(filename, std::ios::ate | std::ios::binary);
-    if(!file.is_open()){
-        throw std::runtime_error("failed to open file");
-    }
-
-    size_t fileSize = (size_t)file.tellg();
-    std::vector<char> buffer(fileSize);
-
-    file.seekg(0);
-    file.read(buffer.data(), fileSize);
-
-    printDebugLog("reading file:", 0, 0);
-    printDebugLog(filename, 1, 1);
-    printDebugLog("buffer size:", 0, 0);
-    printDebugLog(std::to_string(buffer.size()), 1, 1);
-    printDebugLog("file size:", 0, 0);
-    printDebugLog(std::to_string(fileSize), 1, 2);
-
-    if(buffer.size() != fileSize){
-        throw std::runtime_error("failed to correctly read from file");
-    }
-
-    file.close();
-    return buffer;
-}
-
 bool firstOpen = true;
 
 //TODO: look into the cost of this function, maybe there's no need to open/close it on every function call
