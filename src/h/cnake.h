346e7d11254a80dfa8ac022cc3c63091631234b8
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    yup.

--- src/h/cnake.h
+++ src/h/cnake.h
@@ -3,19 +3,18 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-class CnakeApp{
+struct CnakeApp{
     const char *version = "Cnake 0.0.0";
     const uint32_t WIDTH = 1920;
     const uint32_t HEIGHT = 1080;
 
     GLFWwindow *window;
 
+    CnakeApp() = default;
     CnakeApp(const CnakeApp&) = delete;
     CnakeApp &operator=(const CnakeApp&) = delete;
 
+    void run();
     void initWindow();
     void gameLoop();
- public:
-    CnakeApp() = default;
-    void run();
 };
