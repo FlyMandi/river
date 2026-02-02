3b6d6c0ac0f956fcfe4cb46a002e2a592e4e28b8
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:37 +0100

    add frame buffers

--- src/engine.cpp
+++ src/engine.cpp
@@ -44,13 +44,14 @@ VkExtent2D swapChainExtent;
 
 std::vector<VkImage> swapChainImages;
 std::vector<VkImageView> swapChainImageViews;
+std::vector<VkFramebuffer> swapChainFramebuffers;
 
 bool appShouldClose(){ return glfwWindowShouldClose(window); }
 
 std::filesystem::path getProjectRoot(){
     std::filesystem::path current = std::filesystem::current_path();
 
-    for(int i = 0; i < 4; ++i){
+    for(int i = 0; i < 3; ++i){
         if(strcmp(current.filename().string().c_str(), appName) == 0) {
             if(config_DEBUG){ std::cout << "project root: " << current << '\n'; }
             return current;
@@ -85,6 +86,7 @@ void initVulkan(){
     createSwapChain();
     createImageViews();
     createGraphicsPipeline();
+    createFramebuffers();
 }
 
 void cleanupVulkan(){
@@ -642,4 +644,25 @@ void createGraphicsPipeline(){
 
     vkDestroyShaderModule(device, vertShaderModule, nullptr);
     vkDestroyShaderModule(device, fragShaderModule, nullptr);
+}
+
+void createFrameBuffers(){
+    swapChainFramebuffers.resize(swapChainImageViews.size());
+
+    for(size_t i = 0; i < swapChainImageViews.size(); ++i){
+        VkImageView attachments[] = { swapChainImageViews[i] };
+
+        VkFramebufferCreateInfo framebufferInfo{};
+        framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        framebufferInfo.renderPass = renderPass;
+        framebufferInfo.attachmentCount = 1;
+        framebufferInfo.pAttachments = attachments;
+        framebufferInfo.width = swapChainExtent.width;
+        framebufferInfo.height = swapChainExtent.height;
+        framebufferInfo.layers = 1;
+
+        if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS){
+            throw std::runtime_error("failed to create framebuffer!");
+        }
+    }
 }
