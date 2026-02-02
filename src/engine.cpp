74ef8b77e9f1483b93dec1d6c75c9d1614a36de5
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:42:34 +0100

    device refactor

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <cstdint>
 #include <filesystem>
-#include <set>
 
 void engine::createInstance(){
     if(build_DEBUG && !checkValidationLayerSupport()){
@@ -169,48 +168,6 @@ bool engine::checkInstanceExtensions(std::vector<const char*> *requiredExt, std:
     }
 
     return true;
-}
-
-
-void engine::createLogicalDevice(){
-    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
-
-    std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
-    std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
-
-    float queuePriority = 1.0f;
-    for(uint32_t queueFamily : uniqueQueueFamilies){
-        VkDeviceQueueCreateInfo queueCreateInfo{};
-        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-        queueCreateInfo.queueFamilyIndex = queueFamily;
-        queueCreateInfo.queueCount = 1;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
-        queueCreateInfos.emplace_back(queueCreateInfo);
-    }
-
-    VkPhysicalDeviceFeatures deviceFeatures{};
-
-    VkDeviceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    createInfo.pQueueCreateInfos = queueCreateInfos.data();
-    createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
-    createInfo.pEnabledFeatures = &deviceFeatures;
-    createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
-    createInfo.ppEnabledExtensionNames = deviceExtensions.data();
-
-    if(build_DEBUG){
-        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-        createInfo.ppEnabledLayerNames = validationLayers.data();
-    }else{
-        createInfo.enabledLayerCount = 0;
-    }
-
-    if(vkCreateDevice(physicalDevice, &createInfo, nullptr, &device)){
-        throw std::runtime_error("failed to create logical device!");
-    }
-
-    vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
-    vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
 }
 
 void engine::createSurface(){
