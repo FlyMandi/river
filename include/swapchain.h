c2b2597b38739ee3a48b6e3acf760bcb34158aa4
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:08 +0100

    add FrameResource

--- include/swapchain.h
+++ include/swapchain.h
@@ -8,15 +8,38 @@ inline VkSwapchainKHR swapchain;
 inline VkFormat swapChainImageFormat;
 inline VkExtent2D swapChainExtent;
 
-inline std::vector<VkImage> swapChainImages;
-inline std::vector<VkImageView> swapChainImageViews;
-inline std::vector<VkFramebuffer> swapChainFramebuffers;
+inline std::vector<VkImage> swapchainImages;
+inline std::vector<VkImageView> swapchainImageViews;
+inline std::vector<VkFramebuffer> swapchainFramebuffers;
 
-extern void createSwapChain();
+constexpr size_t maxFramebuffers = 1;
+constexpr size_t maxBuffers = 1;
+constexpr size_t maxPipelines = 1;
+constexpr size_t maxRenderPasses = 1;
+
+struct FrameResource
+{
+    VkFramebuffer framebuffers2Destroy[maxFramebuffers];
+    size_t framebufferCount;
+
+    VkBuffer buffers2Destroy[maxBuffers];
+    size_t bufferCount;
+
+    VkPipeline pipelines2Destroy[maxPipelines];
+    size_t pipelineCount;
+
+    VkRenderPass renderPasses2Destroy[maxRenderPasses];
+    size_t renderPassCount;
+};
+
+constexpr uint16_t MAX_FRAMES_IN_FLIGHT = 2;
+inline FrameResource frameResources[MAX_FRAMES_IN_FLIGHT];
+
+extern void createSwapchain();
 extern void createImageViews();
 extern void createRenderPass();
 
-extern void cleanupSwapChain();
-extern void recreateSwapChain();
+extern void cleanupSwapchain();
+extern void recreateSwapchain();
 
 extern VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
