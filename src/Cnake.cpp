5b60799127e769f3cb5e03cffb2954c60b0430bd
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    add vulkan instance

--- src/Cnake.cpp
+++ src/Cnake.cpp
@@ -1,3 +1,4 @@
+#include "vulkan/vulkan_core.h"
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -11,6 +12,7 @@ class CnakeApp{
     const uint32_t HEIGHT = 1080;
 
     GLFWwindow *window;
+    VkInstance instance;
 
     void initWindow(){
         glfwInit();
@@ -21,8 +23,33 @@ class CnakeApp{
         window = glfwCreateWindow(WIDTH, HEIGHT, version, nullptr, nullptr);
     }
 
-    void initVulkan(){
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Cnake";
+        appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if(vkCreateInstance(&createInfo, nullptr, &instance)){
+            throw std::runtime_error("failed to create instance.");
+        }
+    }
+
+    void initVulkan(){
+        createInstance();
     }
 
     void gameLoop(){
