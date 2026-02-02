f887fe1e7ecbc7f1c003e11988c003370582435b
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    separate build & run

--- run.ps1
+++ run.ps1
@@ -1,6 +1,3 @@
-param( $config = "debug" )
-&premake5 clean
-&premake5 vs2022
-.\src\win_compile_shaders.ps1
-&"$env:ProgramFiles\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" .\build\Cnake.sln -t:Rebuild -p:Configuration=$config
+param ( $config = "debug" )
 &".\bin\$config\Cnake.exe"
+
