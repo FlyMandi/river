e8761ab93c22366647a9e4994d376733b0dd493e
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:08 +0100

    BUGFIX: vert attr wrong format

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -34,7 +34,7 @@ std::array<VkVertexInputAttributeDescription, 2> getVertexAttributeDescriptions(
 
     attributeDescriptions[1].binding = 0;
     attributeDescriptions[1].location = 1;
-    attributeDescriptions[1].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
     attributeDescriptions[1].offset = offsetof(Vertex, color);
 
     return attributeDescriptions;
