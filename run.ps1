373b8457d1a8f0c56a6fe849b6bacc8c5dedce4f
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:07 +0100

    add simple .\run script

---
+++ run.ps1
@@ -0,0 +1,20 @@
+$Platforms = "Win64", "Unix"
+$Configurations = "Debug", "Release"
+
+foreach($platform in $Platforms){
+    foreach($config in $Configurations){
+        $objPath = ".\obj\$platform" + "_$config"
+        $binPath = ".\bin\$platform" + "_$config"
+        if(-Not(Test-Path $objPath)){
+            &mkdir $objPath
+        }
+        if(-Not(Test-Path $binPath)){
+            &mkdir $binPath
+        }
+    }
+}
+
+&premake5 gmake
+Push-Location ".\build\"
+&make
+Pop-Location
