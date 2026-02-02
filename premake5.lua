6a501d3d606a3164c1558e18c06ddd07a24d4f75
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:35 +0100

    fix linking issues with VS2022

--- premake5.lua
+++ premake5.lua
@@ -3,24 +3,34 @@ VULKAN_SDK = os.getenv("VULKAN_SDK")
 
 workspace("Cnake")
 configurations({ "Debug", "Release" })
+platforms({ "Win64" })
 location("build")
 
 project("Cnake")
-kind("WindowedApp")
+kind("ConsoleApp")
 language("C++")
 cppdialect("C++20")
 targetdir("bin/%{cfg.buildcfg}")
-includedirs({ "../lib/", "../lib/GLFW/include", "%{VULKAN_SDK}/Include/" })
+includedirs({ "../lib/", "../lib/GLFW/include/", "%{VULKAN_SDK}/Include/" })
+syslibdirs({ "%{VULKAN_SDK}/Lib/", "../lib/GLFW/lib-vc2022/" })
 
 files({ "**.h", "**.cpp" })
 
 filter("configurations:Debug")
 defines({ "DEBUG" })
 symbols("On")
+ignoredefaultlibraries({ "MSVCRT" })
 
 filter("configurations:Release")
 defines({ "NDEBUG" })
 optimize("On")
+
+filter("platforms:Win64")
+system("Windows")
+architecture("x86_64")
+
+filter("action:vs*")
+linkoptions({ "vulkan-1.lib", "glfw3.lib" })
 
 newaction({
 	trigger = "clean",
@@ -29,6 +39,9 @@ newaction({
 		print("clean the build...")
 		os.rmdir("./build/")
 		os.rmdir("./bin/")
+		os.remove("BuildRules.xml")
+		os.remove("BuildRules.props")
+		os.remove("BuildRules.targets")
 		print("done.")
 	end,
 })
