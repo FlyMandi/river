15e6cf544e85d03a3107f894c46c1d3ecd1e4682
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:09 +0100

    fill out imageCreateInfo

--- include/image.h
+++ include/image.h
@@ -1,3 +1,8 @@
 #pragma once
 
+#include "vulkan/vulkan_core.h"
+
 extern void createTextureImage();
+
+inline VkImage textureImage;
+inline VkDeviceMemory textureImageMemory;
--- src/image.cpp
+++ src/image.cpp
@@ -51,4 +51,17 @@ void createTextureImage()
     vkUnmapMemory(logicalDevice, stagingBufferMemory);
 
     stbi_image_free(pixels);
+
+    VkImageCreateInfo imageCreateInfo{};
+    imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+    imageCreateInfo.extent.width = static_cast<uint32_t>(texWidth);
+    imageCreateInfo.extent.height = static_cast<uint32_t>(texHeight);
+    imageCreateInfo.extent.depth = 1;
+    imageCreateInfo.mipLevels = 1;
+    imageCreateInfo.arrayLayers = 1;
+    imageCreateInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+    imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+
+    //FIXME:#50: WIP
 }
