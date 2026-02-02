e0fe6f858fd68a898f907a8ddfc8a6e9bd5204a5
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:43:07 +0100

    fix & expand WinMain

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
+#include "river.h"
 #include "window.h"
 #include "editor.h"
 
+#ifndef _WIN32
+
 int main(){
-    initGLFW();
     initEditor();
+    initGLFW();
+    initVulkan();
 
     loopEditor();
 
     cleanupEditor();
     cleanupGLFW();
+    cleanupVulkan();
+
+    return 0;
 }
+
+#else
+
+int WinMain(){
+    initEditor();
+    initGLFW();
+    initVulkan();
+
+    loopEditor();
+
+    cleanupEditor();
+    cleanupGLFW();
+    cleanupVulkan();
+
+    return 0;
+}
+
+#endif
