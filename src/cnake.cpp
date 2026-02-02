6b8d674bedc9bf1e6609efedde35e64d6f08d008
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    separate cnake, engine, init

--- src/cnake.cpp
+++ src/cnake.cpp
@@ -1,225 +1,9 @@
 #include "h/cnake.h"
+#include "h/engine.h"
 #include "GLFW/glfw3.h"
-#include "vulkan/vulkan_core.h"
 
-#include <map>
-#include <stdexcept>
 #include <cstdlib>
-#include <vector>
 #include <cstring>
-
-VkResult CreateDebugUtilsMessengerEXT(
-        VkInstance                                  instance,
-        const VkDebugUtilsMessengerCreateInfoEXT    *pCreateInfo,
-        const VkAllocationCallbacks                 *pAllocator,
-        VkDebugUtilsMessengerEXT                    *pDebugMessenger
-    ){
-
-    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
-    if(nullptr != func){
-        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    }else{
-        return VK_ERROR_EXTENSION_NOT_PRESENT;
-    }
-}
-
-void DestroyDebugUtilsMessengerEXT(
-        VkInstance                  instance,
-        VkDebugUtilsMessengerEXT    messenger, 
-        const VkAllocationCallbacks *pAllocator
-    ){
-
-    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
-    if(nullptr != func){
-        func(instance, messenger, pAllocator);
-    }
-}
-
-void CnakeApp::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo){
-    createInfo = {}; 
-    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.messageSeverity =    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT; 
-    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-    createInfo.pfnUserCallback = debugCallback;
-}
-
-void CnakeApp::setupDebugMessenger(){
-    if(!enableValidationLayers){ return; }
-
-    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
-    populateDebugMessengerCreateInfo(createInfo);
-
-    if(CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger)){
-        throw std::runtime_error("failed to set up debug messenger!");
-    }
-}
-
-std::vector<const char*> CnakeApp::getRequiredExtensions(){
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
-
-    if(enableValidationLayers){
-        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    }
-
-    return extensions;
-}
-
-bool CnakeApp::checkValidationLayerSupport(){
-    uint32_t layerCount;
-    
-    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-    std::vector<VkLayerProperties> layerVec(layerCount);
-    vkEnumerateInstanceLayerProperties(&layerCount, layerVec.data());
-
-    for(const char *layer : validationLayers){
-        bool layerFound = false;
-
-        for(const auto &layerPresent : layerVec){
-            if(0 == strcmp(layerPresent.layerName, layer)){
-                layerFound = true;
-                break;
-            }
-        }
-        if(!layerFound){ return false; }
-    }
-
-    return true;
-}
-
-bool CnakeApp::checkExtensionSupport(std::vector<const char*> *requiredExt, std::vector<VkExtensionProperties> *instanceExt){
-    std::cout << "\n\tInstance presents:\n";
-    for(const auto &extension : *instanceExt){
-        std::cout << "\t" << extension.extensionName << '\n';
-    }
-
-    std::cout << "\n\tRequired:\n"; 
-    for(const auto &required : *requiredExt){
-        bool extFound = false;
-        
-            for(const auto &present : *instanceExt){
-                if(0 == strcmp(required, present.extensionName)){
-                    std::cout << "found:\t" << required << '\n';
-                    extFound = true;
-                    break;
-                }
-            }
-        if(!extFound){ 
-            std::cout << "not found: \t" << required << '\n';
-            return false; 
-        } 
-    }
-
-    return true;
-}
-
-void CnakeApp::initWindow(){
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-    window = glfwCreateWindow(WIDTH, HEIGHT, version, nullptr, nullptr);
-}
-
-void CnakeApp::createInstance(){
-    if(enableValidationLayers && !checkValidationLayerSupport()){
-        throw std::runtime_error("validation layers requested, but not available!");
-    }
-
-    VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Cnake";
-    appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
-
-    vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionCount, nullptr);
-    std::vector<VkExtensionProperties> instanceExtensions(instanceExtensionCount); 
-    vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionCount, instanceExtensions.data());
-
-    auto requiredExtensions = getRequiredExtensions();
-    if(!checkExtensionSupport(&requiredExtensions, &instanceExtensions)){
-        throw std::runtime_error("extensions required, but not available!"); 
-    }
-
-    std::cout << "\nAll needed extensions are present.\n\n";
-
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-    createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
-    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
-    createInfo.enabledLayerCount = 0;
-
-    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
-    if(enableValidationLayers){
-        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size()); 
-        createInfo.ppEnabledLayerNames = validationLayers.data();
-        
-        populateDebugMessengerCreateInfo(debugCreateInfo);
-        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
-    }else{
-        createInfo.enabledLayerCount = 0;
-        createInfo.pNext = nullptr;
-    }
-
-    if(vkCreateInstance(&createInfo, nullptr, &instance)){
-        throw std::runtime_error("failed to create instance.");
-    }
-}
-
-uint32_t CnakeApp::rateDeviceSuitability(VkPhysicalDevice device){
-    uint32_t score = 0;
-    vkGetPhysicalDeviceProperties(device, &deviceProperties);
-    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-
-    if(deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU){
-        score += 1000;
-    }
-    score += deviceProperties.limits.maxImageDimension2D;
-
-    if(!deviceFeatures.geometryShader){ return 0; }
-
-    return score;
-}
-
-void CnakeApp::pickPhysicalDevice(){
-    uint32_t deviceCount = 0;
-    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-    
-    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-    if(0 == deviceCount){
-        throw std::runtime_error("failed to find any GPU with vulkan support!");
-    }
-
-    std::vector<VkPhysicalDevice> devices(deviceCount);
-    std::multimap<uint32_t, VkPhysicalDevice> suitabilityCandidates;
-
-    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-
-    for(const auto& device : devices){
-        uint32_t score = rateDeviceSuitability(device);
-        suitabilityCandidates.insert(std::make_pair(score, device));
-    }
-
-    if(suitabilityCandidates.rbegin()->first > 0){
-        physicalDevice = suitabilityCandidates.rbegin()->second; 
-    }else{
-        throw std::runtime_error("failed to find a suitable GPU!");
-    }
-}
-
-void CnakeApp::initVulkan(){
-    createInstance();
-    setupDebugMessenger();
-    pickPhysicalDevice();
-}
 
 void CnakeApp::gameLoop(){
     while(!glfwWindowShouldClose(window)){
@@ -227,20 +11,25 @@ void CnakeApp::gameLoop(){
     }
 }
 
-void CnakeApp::cleanup(){
-    if(enableValidationLayers){
-        DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
-    }
-
-    vkDestroyInstance(instance, nullptr);
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-}
-
 void CnakeApp::run(){
     initWindow();
-    initVulkan();
+
+    Engine engine;
+    engine.initVulkan();
+
     gameLoop();
-    cleanup();
+    engine.cleanup();
+}
+
+int main(){
+    CnakeApp app;
+
+    try{
+        app.run();
+    }catch(const std::exception &e){
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
--- src/h/cnake.h
+++ src/h/cnake.h
@@ -1,66 +1,22 @@
 #pragma once
 
-#include <cstdint>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include "vulkan/vulkan_core.h"
 
-#include <vector>
-#include <iostream>
+#include <cstdint>
 
 class CnakeApp{
     const char *version = "Cnake 0.0.0";
     const uint32_t WIDTH = 1920;
     const uint32_t HEIGHT = 1080;
 
-    uint32_t instanceExtensionCount = 0;
-    uint32_t glfwExtensionCount = 0;
-
-    const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
-
-#ifdef NDEBUG
-    const bool enableValidationLayers = false;
-#else 
-    const bool enableValidationLayers = true;
-#endif
-
     GLFWwindow *window;
-    VkInstance instance;
 
-    VkDebugUtilsMessengerEXT debugMessenger;
-
-    VkPhysicalDeviceProperties deviceProperties;
-    VkPhysicalDeviceFeatures deviceFeatures;
-
-    std::vector<const char*> getRequiredExtensions();
-    
-    bool checkExtensionSupport(std::vector<const char*> *requiredExt, std::vector<VkExtensionProperties> *instanceExt);
-    bool checkValidationLayerSupport();
-
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
-    void setupDebugMessenger();
-
-    void pickPhysicalDevice();      
-    uint32_t rateDeviceSuitability(VkPhysicalDevice device);
-
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-            VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
-            VkDebugUtilsMessageTypeFlagsEXT             messageType, 
-            const VkDebugUtilsMessengerCallbackDataEXT  *pCallbackData,
-            void                                        *pUserData
-        ){
-
-        std::cerr << "validation layer: " << pCallbackData->pMessage << '\n';
-
-        return VK_FALSE;
-    }
+    //TODO: remove copy constructor & operator
+    //add default constructor that binds CnakeApp to Engine
 
     void initWindow();
-    void createInstance();
-    void initVulkan();
     void gameLoop();
-    void cleanup();
-
  public:
     void run();
 };
