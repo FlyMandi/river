9b822003d776c0bcd5c7506f61cbce370ec3dffc
FlyMandi <terribleacronym@gmail.com>
2026-02-02 10:37:36 +0100

    remove auto

--- src/Engine_Pipeline.cpp
+++ src/Engine_Pipeline.cpp
@@ -39,8 +39,8 @@ void EnginePipeline::createGraphicsPipeline(const PipelineConfigInfo &configInfo
     assert(configInfo.pipelineLayout != VK_NULL_HANDLE && "Cannot create graphics pipeline: no pipelineLayout provided in configInfo");
     assert(configInfo.renderPass != VK_NULL_HANDLE && "Cannot create graphics pipeline: no renderPass provided in configInfo");
 
-    auto vertCode = readFile(vertFilepath);
-    auto fragCode = readFile(fragFilepath);
+    std::vector<char> vertCode = readFile(vertFilepath);
+    std::vector<char> fragCode = readFile(fragFilepath);
 
     createShaderModule(vertCode, &vertShaderModule);
     createShaderModule(fragCode, &fragShaderModule);
